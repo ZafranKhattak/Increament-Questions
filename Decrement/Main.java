29aa12b3828b1cda3ac113b8805616bb67c2d848
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-07-22 23:57:37 +0500

    First commit

--- Decrement/Main.java
+++ Decrement/Main.java
@@ -2,8 +2,8 @@ package Decrement;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-int b = --a;
+int a = 5;
+int b = a--;
 
 System.out.println(a);
 System.out.println(b);
