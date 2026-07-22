6e300dbc115abaf5dd9e9e469658a8bfe2d15656
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-07-22 23:48:19 +0500

    First Commit

---
+++ PreIncrement/Question2/Main.java
@@ -0,0 +1,11 @@
+package PreIncrement.Question2;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = ++a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
