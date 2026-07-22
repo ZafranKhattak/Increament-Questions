6d0c29e711725c269b2d50e2330f97d8f38238ca
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-07-22 23:54:05 +0500

    First Commit

---
+++ Question/Main.java
@@ -0,0 +1,11 @@
+package Question;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+int b = a++ + ++a;
+
+System.out.println(a);
+System.out.println(b);
+    }
+}
