f24c1ee8a8c2d9beb9d1acd2928bd0617107e05b
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-07-22 23:55:21 +0500

    First Commit

---
+++ add1/Main.java
@@ -0,0 +1,11 @@
+package add1;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a++;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
